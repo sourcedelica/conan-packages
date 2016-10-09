c1180270df28118cdfa71c39551b7a1cbfb03a9d
Eric Pederson <ericacm+github@gmail.com>
2016-10-09 01:39:49 -0400

    formatting tweaks

--- caf/conanfile.py
+++ caf/conanfile.py
@@ -1,7 +1,6 @@
 from conans import ConanFile, CMake
 import os
 import sys
-import pdb
 
 version_env = "CONAN_PACKAGE_VERSION"
 version_str = os.getenv(version_env)
@@ -24,21 +23,18 @@ class CAFConan(ConanFile):
         self.run("cd %s && git checkout %s" % (self.source_dir, self.version))
 
     def build(self):
-	static_suffix = "" if self.options.shared else "-only"
-        # TODO - get build type from settings
-	configure = "./configure --build-type=RelWithDebInfo --no-python --no-examples --no-opencl --no-tools --no-unit-tests --build-static%s" % static_suffix
-	self.run("cd %s && %s" % (self.source_dir, configure))
-	self.run("cd %s && make" % self.source_dir)
+        static_suffix = "" if self.options.shared else "-only"
+        configure = \
+            "./configure --no-python --no-examples --no-opencl --no-tools --no-unit-tests --build-static%s" % \
+            static_suffix
+        self.run("cd %s && %s" % (self.source_dir, configure))
+        self.run("cd %s && make" % self.source_dir)
 
     def package(self):
-        cd = os.path.realpath(os.curdir)
-#        pdb.set_trace()
-	
-        self.copy("*.hpp", dst="include/caf", src="%s/libcaf_core/caf" % self.source_dir)
-        self.copy("*.hpp", dst="include/caf", src="%s/libcaf_io/caf" % self.source_dir)
-        self.copy("*.lib", dst="lib", src="%s/build/lib" % self.source_dir)
-        self.copy("*.dylib", dst="lib", src="%s/build/lib" % self.source_dir)
-        self.copy("*.a", dst="lib", src="%s/build/lib" % self.source_dir)
+        self.copy("*.hpp",   dst="include/caf", src="%s/libcaf_core/caf" % self.source_dir)
+        self.copy("*.hpp",   dst="include/caf", src="%s/libcaf_io/caf" % self.source_dir)
+        self.copy("*.dylib", dst="lib",         src="%s/build/lib" % self.source_dir)
+        self.copy("*.a",     dst="lib",         src="%s/build/lib" % self.source_dir)
 
     def package_info(self):
         self.cpp_info.libs = ["caf_io_static", "caf_core_static"]
