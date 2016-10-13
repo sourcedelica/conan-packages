899668ac12acaed084e18ebeb8c776e530d3ef03
Eric Pederson <ericacm+github@gmail.com>
2016-10-12 22:32:40 -0400

    delete tests dir - it will be generated

--- caf/test_package/conanfile.py
+++ caf/test_package/conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake
+from conans.util.files import save
 import os
 import sys
 
@@ -20,9 +21,23 @@ class CAFReuseConan(ConanFile):
 
     def build(self):
         # TODO - copy tests from package source to tests dir
+        """
+        rm -rf sparse-caf
+        mkdir sparse-caf
+        cd sparse-caf
+        git init
+        git remote add origin https://github.com/actor-framework/actor-framework.git
+        git config core.sparseCheckout true
+        save(".git/info/sparse-checkout", "libcaf_test")
+        save(".git/info/sparse-checkout", "libcaf_core/test", True)
+        save(".git/info/sparse-checkout", "libcaf_io/test", True)
+        git pull origin %s --depth 1 % version
+        # copy files from sparse-caf
+        """
         cmake = CMake(self.settings)
         self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
+
 
     def test(self):
         self.run(os.sep.join([".","bin", "caf-test"]))
