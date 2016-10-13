d8097c5232fb13ea6676ab4f58e8889ad11ea0e0
Eric Pederson <ericacm+github@gmail.com>
2016-10-12 22:44:48 -0400

    Adjust test_package/CMakeLists.txt for new test setup

--- caf/test_package/conanfile.py
+++ caf/test_package/conanfile.py
@@ -20,24 +20,20 @@ class CAFReuseConan(ConanFile):
     generators = "cmake"
 
     def build(self):
-        # TODO - copy tests from package source to tests dir
-        """
-        rm -rf sparse-caf
-        mkdir sparse-caf
-        cd sparse-caf
-        git init
-        git remote add origin https://github.com/actor-framework/actor-framework.git
-        git config core.sparseCheckout true
-        save(".git/info/sparse-checkout", "libcaf_test")
-        save(".git/info/sparse-checkout", "libcaf_core/test", True)
-        save(".git/info/sparse-checkout", "libcaf_io/test", True)
-        git pull origin %s --depth 1 % version
-        # copy files from sparse-caf
-        """
+        tests_dir = "tests"
+        repo_url = "https://github.com/actor-framework/actor-framework.git"
+        self.run("rm -rf %s" % tests_dir)
+        self.run("git init %s" % tests_dir)
+        self.run("cd %s && git remote add origin %s" % (tests_dir, repo_url))
+        self.run("cd %s && git config core.sparseCheckout true" % tests_dir)
+        save("%s/.git/info/sparse-checkout" % tests_dir, "libcaf_test")
+        save("%s/.git/info/sparse-checkout" % tests_dir, "libcaf_core/test", True)
+        save("%s/.git/info/sparse-checkout" % tests_dir, "libcaf_io/test", True)
+        self.run("cd %s && git pull origin %s --depth 1" % (tests_dir, self.version))
+
         cmake = CMake(self.settings)
         self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
-
     def test(self):
-        self.run(os.sep.join([".","bin", "caf-test"]))
+        self.run(os.sep.join([".", "bin", "caf-test"]))
